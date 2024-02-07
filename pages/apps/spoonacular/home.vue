c00c3f39d780830e7403d2ed40f4e9c203efd9ec
sriram-500apps <sriram.pothuraju@500apps.com>
2024-02-07 07:22:48 +0530

    random recipes service added to home

--- pages/apps/spoonacular/home.vue
+++ pages/apps/spoonacular/home.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
+import type { Recipe } from '~/types/spoonacular';
+
 definePageMeta({
   layout: 'spoonacular',
 })
+const response:Ref<Recipe> = await $fetch('/api/spoonacular/recipes')
+console.log("hello", response)
 </script>
 
 <template>
   <SpoonacularRecipesList />
+  <ul>
+    <li v-for="recipe in response">
+      {{ recipe.title }}
+    </li>
+  </ul>
 </template>
