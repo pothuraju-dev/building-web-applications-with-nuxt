c578b2e90993c6809bb8ed6d3f3476d2bea6f563
sriram-500apps <sriram.pothuraju@500apps.com>
2024-02-07 08:06:00 +0530

    kept comic in home

--- pages/apps/spoonacular/home.vue
+++ pages/apps/spoonacular/home.vue
@@ -4,9 +4,9 @@ import type { Recipe } from '~/types/spoonacular';
 definePageMeta({
   layout: 'spoonacular',
 })
-const recipe = await $fetch(`/api/spoonacular/recipes`, {})
+// const recipe = await $fetch(`/api/spoonacular/recipes`, {})
 const comics = await $fetch(`/api/marvel/comics`, {})
-console.log("recipe", recipe)
+// console.log("recipe", recipe)
 console.log("comics", comics)
 
 </script>
@@ -14,6 +14,6 @@ console.log("comics", comics)
 <template>
   <SpoonacularRecipesList />
   <ul>
-      {{ recipe }}
+      {{ comics }}
   </ul>
 </template>
