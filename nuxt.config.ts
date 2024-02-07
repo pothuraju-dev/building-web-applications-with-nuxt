0fcc3454ee7f3ac9f44e78ae598aa12e2efc670a
sriram-500apps <sriram.pothuraju@500apps.com>
2024-02-07 07:44:33 +0530

    route rules added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/test-utils/module', '@nuxtjs/tailwindcss', '@pinia/nuxt'],
+  modules: ['@nuxt/test-utils/module', '@nuxtjs/tailwindcss', '@pinia/nuxt','@tresjs/nuxt', '@nuxt/devtools'],
+  tres:{
+    devtools: true,
+    glsl: true,
+  },
   imports: {
     dirs: ['stores'],
   },
   pinia: {
     autoImports: ['defineStore', ['defineStore', 'definePiniaStore']],
+  },
+  routeRules: {
+    '/**': {
+      isr: 60 * 60 * 24
+    }
   },
 })
