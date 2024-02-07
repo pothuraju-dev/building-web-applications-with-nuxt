68602b1f7d1eb2c8c93bfb1a8ceb32ec1b87133c
sriram-500apps <sriram.pothuraju@500apps.com>
2024-02-07 08:00:06 +0530

    removed spoonacular servers

---
+++ server/api/spoonacular/[spoonacularSlug].ts
@@ -0,0 +1,24 @@
+import { makeAuthenticatedAPICall } from '~/server/utils/marvel'
+import { Recipe } from '~/types/spoonacular'
+
+export default defineEventHandler(async (event) => {
+  // Define your API endpoint for marvel information
+  const { spoonacularSlug } = getRouterParams(event)
+
+  const apiKey = process.env.APP_SPOONACULAR_API
+  const spoonacularAPI = 'https://api.spoonacular.com/recipes/random'
+  // Create a function to fetch marvel information
+
+  // Todo: getPagination
+
+  // Todo: getQuery
+  try {
+    const { recipes } = await $fetch(`${spoonacularAPI}/?apiKey=${apiKey}`) as {recipes: Recipe[]}
+    // Return the marvel data
+    return recipes[0]
+  } catch (error) {
+    // Handle errors here
+    console.error('Error fetching marvel:', error)
+    throw new Error('Failed to fetch marvel information')
+  }
+})
