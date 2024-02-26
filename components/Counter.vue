c3612efa7c77ec5cd596780a796e57cc4a54f5df
sriram-500apps <sriram.pothuraju@500apps.com>
2024-02-26 23:18:39 +0530

    reactivity-loss-in-props

---
+++ components/Counter.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>Count: {{ reactiveCount }}</p>
+  </div>
+</template>
+
+// Counter.vue
+<script setup>
+import { toRef } from 'vue'
+
+const props = defineProps({
+  count: Number,
+})
+
+const reactiveCount = toRef(() => props.count)
+</script>
---
+++ pages/articles/reactivity-loss-in-props.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>
+      <Counter :count="count" />
+      <button @click="count++">Increment</button>
+    </div>
+    <div>
+      <p>
+        <a :href="githubLink" target="_blank">View Component File on GitHub</a>
+      </p>
+      <div id="gist-section">
+        <GistView></GistView>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
